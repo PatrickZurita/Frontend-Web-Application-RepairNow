e8893663d197d8a746f3d3798acf9926648c3706
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2022-09-25 03:45:28 -0500

    Nav Bar Simply

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,16 +1,19 @@
 <template>
 
 <nav>
-    <pv-toolbar class="bg-primary-reverse">
+    <pv-toolbar class="bg-primary">
         
-        <template #start>
+        <template #start>   
             <h1>Repair Now</h1>
         </template>
 
         <template #end>
             <div class="flex flex-wrap justify-content-center gap-3">
                 <router-link to="/">Home</router-link>
-                <router-link to="/about">About</router-link>
+                <router-link to="/MyAppliance">My Appliances</router-link>
+                <router-link to="/MyPlan">My Plan</router-link>
+                <router-link to="/Notifications">Notifications</router-link>
+                <router-link to="/Profile">Profile</router-link>
             </div>
         </template>
 
--- src/router/router.js
+++ src/router/router.js
@@ -2,10 +2,18 @@ import {createRouter,createWebHistory} from "vue-router"
 /*Componenets*/
 import Home from '../components/Home.vue'
 import NavigationBar from '../components/NavigationBar.vue'
+import MyAppliance from '../components/Client/MyAppliance.vue'
+import MyPlan from '../components/Client/MyPlan.vue'
+import Notifications from '../components/Client/Notifications.vue'
+import Profile from '../components/Client/Profile.vue'
+
 
 const routes = [
     { path: '/', component: Home },
-    { path: '/NavigationBar', component: NavigationBar }
+    { path: '/MyAppliance', component: MyAppliance },
+    { path: '/MyPlan', component: MyPlan },
+    { path: '/Notifications', component: Notifications },
+    { path: '/Profile', component: Profile },
 ]
 
 const history=createWebHistory()

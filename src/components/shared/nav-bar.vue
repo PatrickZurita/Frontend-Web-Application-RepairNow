f1237be5132f414ca1aefaf08e8adb703ece3820
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2022-09-29 19:02:47 -0500

    Add nav-bar  responsive

--- src/components/shared/nav-bar.vue
+++ src/components/shared/nav-bar.vue
@@ -1,4 +1,6 @@
 <template>
+    <TabMenu class="topnav" :model="items" />
+
 
     <pv-toolbar>
         <template #start>
@@ -48,13 +50,15 @@
 export default {
     data() {
         return {
-            number:1
+            number:1,
+            items: [
+                {label: 'Home', icon: 'pi pi-fw pi-home'},
+                {label: 'Sign Up Now', icon: 'pi pi-fw pi-calendar'},
+                {label: 'Loggin', icon: 'pi pi-fw pi-pencil'},
+            ]
         }
     },
     methods: {
-        changeBar(){
-            this.number+=1;
-        },
         redirectSignUp(){
             this.$router.push("/register")
         },
@@ -69,5 +73,36 @@ export default {
 </script>
 
 <style>
+
+    .topnav {
+        background-color: #333;
+        overflow: hidden;
+        align-content: right;
+        align-items: right;
+      }
+      
+      .topnav a {
+        float: right;
+        display: block;
+        color: #f2f2f2;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+      }
+     
+      .topnav a:hover {
+        background-color: #ddd;
+        color: black;
+      }
+      
+      .topnav a.active {
+        background-color: #04AA6D;
+        color: white;
+      }
+
+      .topnav .icon {
+        display: none;
+      }
 
 </style>

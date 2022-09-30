0c19881d2bab1cbebcb6d447d0d7a86eebcf3639
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2022-09-29 19:00:21 -0500

    Add register responsive

--- src/components/shared/register.vue
+++ src/components/shared/register.vue
@@ -7,21 +7,23 @@
     <div class="Two">
 
         <div class="cards">
+            <div class="column">
+                <pv-card class="card">
+                    <template #title>
+                        Client!
+                    </template>
+                    <template #content>
+                        You are a client
+                    </template>
+                    <template #footer>
+                        <img src="@/assets/img/user.png" width="100">
+                        <pv-button label="Register" @click="redirectRegisterClient"></pv-button>
+                    </template>
+                </pv-card>
+            </div>
 
-            <pv-card>
-                <template #title>
-                    Client!
-                </template>
-                <template #content>
-                    You are a client
-                </template>
-                <template #footer>
-                    <img src="@/assets/img/user.png" width="100">
-                    <pv-button label="Register" @click="redirectRegisterClient"></pv-button>
-                </template>
-            </pv-card>
-
-            <pv-card>
+            <div class="column">
+            <pv-card class="card">
                 <template #title>
                    Technician!
                 </template>
@@ -33,10 +35,13 @@
                     <pv-button label="Register" @click="redirectRegisterTechnician"></pv-button>
                 </template>
             </pv-card>
+            </div>
+
+
 
         </div>
 
-        
+
 
     </div>
 
@@ -65,5 +70,27 @@ export default {
 </script>
 
 <style scoped>
+* {
+    box-sizing: border-box;
+}
+.cards {margin: 0 -5px;}
 
+.cards::after{
+    content: "";
+    display: table;
+    clear: both;
+}
+
+.card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); /* this adds the "card" effect */
+    padding: 16px;
+    text-align: center;
+    background-color: #f1f1f1;
+
+  }
+  .column {
+    float: left;
+    width: 25%;
+    padding: 0 10px;
+  }
 </style>
